78760ad205026df2f3ba61af84c51e201c9675d9
Joren Van Hee <jorenvanhee@gmail.com>
2021-09-30 17:08:46 +0200

    Update xs breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
   darkMode: 'media', // or 'media' or 'class'
   theme: {
     screens: {
-      'xs': '360px',
+      'xs': '450px',
       'sm': '640px',
       'md': '768px',
       'lg': '1024px',
